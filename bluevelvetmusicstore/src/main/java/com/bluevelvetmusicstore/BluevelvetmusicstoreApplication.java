bf60b1f0f58c137f11b96b661dbc1b07cffdfe81
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2024-11-08 22:30:39 -0300

    adding initial files

---
+++ bluevelvetmusicstore/src/main/java/com/bluevelvetmusicstore/BluevelvetmusicstoreApplication.java
@@ -0,0 +1,13 @@
+package com.bluevelvetmusicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluevelvetmusicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluevelvetmusicstoreApplication.class, args);
+	}
+
+}
---
+++ bluevelvetmusicstore/src/test/java/com/bluevelvetmusicstore/BluevelvetmusicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluevelvetmusicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluevelvetmusicstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
